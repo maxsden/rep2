7446fe864c2e70a0f4b81cb083f37d4997da4ee1
olimp <olimp@university.kspu.kaluga.ru>
2022-09-12 18:30:50 +0300

    second commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require ("express");
+const app = express();
+
+const port = 3000;
+app.listen(port,()=> {
+
+    console.log(`Сервер запущен. Порт сервера: ${port}`);
+
+});
+
+app.get("/",(req, res)=>{
+    
+    res.send("Hello world!");
+
+});
